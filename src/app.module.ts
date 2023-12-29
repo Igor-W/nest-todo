73a4265bf08c467a58e4c11e7e6d0ad41c05983d
Igor-W <igori.stoian@gmail.com>
2023-12-29 17:50:31 +0200

    test-updating

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,13 @@ import { join } from 'path';
 import { TodoitemsModule } from './todoitems/todoitems.module';
 
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost:27017/test';
+  process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/todo';
 
 @Module({
   imports: [
     MongooseModule.forRoot(databaseUrl),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
+      rootPath: join(__dirname, '..', 'public', 'browser'),
     }),
     TodoitemsModule,
   ],
